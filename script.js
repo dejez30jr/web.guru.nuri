0ef9877aef452a980712da637dc74ca5426670f8
deris <165728707+dejez30jr@users.noreply.github.com>
2024-08-11 10:25:27 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+window.addEventListener('scroll', function() {
+    var navbar = document.querySelector('.navbar');
+    if (window.scrollY > 0) {
+        navbar.style.top = 'auto';
+        navbar.style.bottom = '0';
+    } else {
+        navbar.style.top = '0';
+        navbar.style.bottom = 'auto';
+    }
+});
+
+
+const text = "WELCOME TO MY WEBSITE SYAHLAN BUDISANTOSO";
+let index = 0;
+const speed = 100; 
+
+function typeWriter() {
+    if (index < text.length) {
+        document.getElementById("typing").innerHTML += text.charAt(index);
+        index++;
+        setTimeout(typeWriter, speed);
+    }
+}
+
+window.onload = typeWriter;
